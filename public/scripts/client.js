6e92e9f49a53a9ccba76f1de37e874aca084f5b4
Mustafa <mustafa31112@gmail.com>
2022-02-03 19:35:15 +0000

    form submission using jquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,48 +5,76 @@
  */
 const data = [
   {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
     },
-    "created_at": 1461116232227
+    created_at: 1461116232227,
   },
   {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
     },
-    "created_at": 1461113959088
-  }
-]
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
 
 
-const renderTweets = function(tweets) {
+
+$(document).ready(function () {
+ 
+  $("#submitform").on("submit", onSubmit)
+  
+  
+  
+  
+  
+  renderTweets(data);
+});
+
+
+
+
+const renderTweets = function (tweets) {
   for (let tweet of tweets) {
-  const element = createTweetElement(tweet)
-  $('#tweets-container').append(element);
+    const element = createTweetElement(tweet);
+    $("#tweets-container").append(element);
   }
+};
+
+const onSubmit = function (event) {
+  console.log("helo")
+  event.preventDefault();
+  const length = $(`#tweet-text`).val().length
+  console.log(length)
+  if (length === 0) {
+    return alert("can not be zero")
   }
 
-  $( "#submitform" ).submit(function( event ) {
-    event.preventDefault();
-  });
+  if (length > 140) {
+    return alert("tweet is long")
+  }
+  
+  //grab input values from the form and save to variables
+  //validations(maybe)
+  //if data is valid send ajax request with serialized input values
+  $.ajax({
+    method: "POST",
+    url: "/tweets",
+    data: $(this).serialize()
+  })
+};
 
-
-
-
-
-
-
-const createTweetElement = function(tweet) {
+const createTweetElement = function (tweet) {
   let $tweet = `<article class="head">
   <header class="tweet-header">
     <div class="image-name">
@@ -73,17 +101,9 @@ const createTweetElement = function(tweet) {
       <p><i class="fas fa-heart"></i></p>
     </div>
   </footer>
-</article>`
-return $tweet;
-}
+</article>`;
+  return $tweet;
+};
 
 
 
-
-
-$( document ).ready(function() {
-  console.log( "ready!" );
-
-  renderTweets(data)
-
-});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,7 +7,7 @@ $("#tweet-text").keyup(function(){
   const counter = $(this).siblings("div").children(".counter");
   counter.val(140 - (this).value.length);
   // counter.text(remaining)
-  console.log(counter)
+ 
   if (remaining < 0) {
     counter.css("color", "red");
   } else {
